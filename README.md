a1a0311dde7c6060e17f00bb31aa54a60f779219
Patrick Costa <patrick.costa@tempest.com.br>
2018-08-23 14:39:39 -0300

    Refactor

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ node index.js password
 
 node index.js car,mouse,house
 ```
+
+This script will generate duplicated lines. You might want to use:
+
+```
+sort -u wordlist.txt > wordlist-sorted.txt
+```
--- modules/index.js
+++ modules/index.js
@@ -1,46 +1,66 @@
 const fs           = require('fs')
 const combinations = require('../util/combinations')
 
-var normalizedPath = require('path').join(__dirname, '.')
+function loadModules () {
 
-var files = require('fs')
-  .readdirSync(normalizedPath)
-  .filter(item => !(/(^|\/)\.[^\/\.]/g).test(item))
+  var normalizedPath = require('path').join(__dirname, '.')
 
-// Create an array with all modules
-var moduleArr = []
-files.forEach(function(file) {
-  if (!fs.statSync(normalizedPath + '/' + file).isDirectory() &&
-      file !== 'index.js')
-  moduleArr.push(file)
-})
+  var files = require('fs')
+    .readdirSync(normalizedPath)
+    .filter(item => !(/(^|\/)\.[^\/\.]/g).test(item))
 
-var core = []
-
-// Process words
-var words = process.argv[2].split(',')
-words.forEach(function (word, i) {
-  core.push(word)
-  combinations(moduleArr).forEach(function (executionList, i) {
-    core.push(executionList.reduce(reducer, word))
+  // Create an array with all modules
+  var moduleArr = []
+  files.forEach(function(file) {
+    if (!fs.statSync(normalizedPath + '/' + file).isDirectory() &&
+        file !== 'index.js')
+    moduleArr.push(file)
   })
-})
 
-function reducer (word, script, index, array) {
-  return require('./' + script)(word)
+  return moduleArr
 }
 
-core.map(string => console.log(string))
+function loadStrings (moduleArr) {
 
-core.map(string => {
-  let sequences = fs.readFileSync(__dirname + '/../sequences.txt')
-  sequences = sequences.toString().split('\n')
-  sequences.splice(-1,1)
-  sequences.map((x) => {
-    console.log(string.concat(x))
+  var core = []
+
+  // Process words
+  var words = process.argv[2].split(',')
+  words.forEach(function (word, i) {
+    core.push(word)
+    combinations(moduleArr).forEach(function (executionList, i) {
+      core.push(executionList.reduce(reducer, word))
+    })
   })
 
-  sequences.map((x) => {
-    console.log(x.toString().concat(string))
+  function reducer (word, script, index, array) {
+    return require('./' + script)(word)
+  }
+
+  return core
+}
+
+function generate (core) {
+
+  core.map(string => console.log(string))
+
+  core.map(string => {
+
+    let sequences = fs
+      .readFileSync(__dirname + '/../sequences.txt')
+      .toString()
+      .split('\n')
+
+    // append
+    sequences.map((x) => {
+      console.log(string.concat(x))
+    })
+
+    // prepend
+    sequences.map((x) => {
+      console.log(x.toString().concat(string))
+    })
   })
-})
+}
+
+generate(loadStrings(loadModules))
